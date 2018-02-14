c3001e7f42c4115de7c625562210490e556886d9
Mathilde FERRIERE <ferriere.mathilde@gmail.com>
2018-02-14 16:31:58 +0100

    amélioration affichage recherche (cacher l'utilisateur des résultats)

--- app/controller/mapController.php
+++ app/controller/mapController.php
@@ -2,20 +2,15 @@
 
 $competences = $compModelDb->getAll();
 
-$recherche = $_POST['recherche'];
-
 if (isset($_POST['recherche'])) {
+    $recherche = $_POST['recherche'];
     $personnes = $personneModelDb->getRecherche($_POST['recherche']);
-
-    $users = [];
-    $user_skills = [];
 
     foreach ($personnes as $personne){
         if (isset($_SESSION['user_id']) && $personne['id'] != $_SESSION['user_id']) {
-            $user_skills = $personneModelDb->getCompetences($personne['id']);
-            $users[] = ['id' => $personne['id'], 'prenom' => $personne['prenom'], 'nom' => $personne['nom'], 'skills' => $user_skills];
+            unset($personnes[$personne['id']]);
         }
     }
+    echo json_encode($personnes);
 }
 
-echo json_encode($personnes);
